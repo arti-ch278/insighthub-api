a36cab13c0c9e19bb594bbfd0e66ee88803cb235
Arti <arti.chourey21@gmail.com>
2026-03-01 21:08:50 +0530

    feat(post): add post service

---
+++ src/main/java/com/artichourey/insighthub/service/PostService.java
@@ -0,0 +1,17 @@
+package com.artichourey.insighthub.service;
+
+import org.springframework.data.domain.Page;
+
+import com.artichourey.insighthub.dtos.PostRequestDto;
+import com.artichourey.insighthub.dtos.PostResponseDto;
+
+public interface PostService {
+	
+	PostResponseDto createPost (PostRequestDto postRequestDto, Long userId, Long categoryId);
+	PostResponseDto updatePost(Long postId, PostRequestDto postRequestDto);
+	PostResponseDto getPostById(Long postId);
+	Page<PostResponseDto> getAllPosts(int pageNumber, int pageSize);
+	void deletePost(Long postId);
+	
+
+}
