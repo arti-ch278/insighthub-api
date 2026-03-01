01094715ddc3c9af8be7a949dd35c317e49eeb4b
Arti <arti.chourey21@gmail.com>
2026-03-01 20:42:26 +0530

    feat(category): add category controller APIs

---
+++ src/main/java/com/artichourey/insighthub/controllers/CategoryController.java
@@ -0,0 +1,72 @@
+package com.artichourey.insighthub.controllers;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.artichourey.insighthub.dtos.CategoryRequestDto;
+import com.artichourey.insighthub.dtos.CategoryResponseDto;
+import com.artichourey.insighthub.payloads.ApiResponse;
+import com.artichourey.insighthub.service.CategoryService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryController {
+	
+	private final CategoryService categoryService;
+	
+	@PostMapping
+    public ResponseEntity<CategoryResponseDto> createCategory(
+            @Valid @RequestBody CategoryRequestDto categoryRequestDto) {
+        CategoryResponseDto createdCategory = categoryService.createCategory(categoryRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryResponseDto> updateCategory(
+            @PathVariable Long categoryId,
+            @Valid @RequestBody CategoryRequestDto categoryRequestDto) {
+        CategoryResponseDto updatedCategory = categoryService.updateCategory(categoryId, categoryRequestDto);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<CategoryResponseDto> getCategoryById(@PathVariable Long categoryId) {
+        CategoryResponseDto category = categoryService.getCategoryById(categoryId);
+        return ResponseEntity.ok(category);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryResponseDto>> getAllCategories(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "10") int pageSize) {
+        
+        Page<CategoryResponseDto> categories = categoryService.getAllCategories(pageNumber, pageSize);
+       
+        return ResponseEntity.ok(categories);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long categoryId) {
+
+        categoryService.deleteCategory(categoryId);
+        return ResponseEntity.ok(new ApiResponse("Category deleted successfully", true));
+    }
+}
+
+
