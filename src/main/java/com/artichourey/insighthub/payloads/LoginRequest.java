b4023768e7a6f18c9ad974e3667ba20b27f76079
Arti <arti.chourey21@gmail.com>
2026-03-05 21:53:42 +0530

    feat: add LoginRequest DTO for handling user login credentials

---
+++ src/main/java/com/artichourey/insighthub/payloads/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.artichourey.insighthub.payloads;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+	
+	private String username;
+	private String password;
+
+}
