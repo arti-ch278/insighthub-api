fa620ddd8d14516098f1bc347a18453aa1e9cdbd
Arti <arti.chourey21@gmail.com>
2026-03-05 22:03:26 +0530

    feat: implement CustomUserDetailsService for loading user authentication data

---
+++ src/main/java/com/artichourey/insighthub/serviceImpl/CustomUserDetailService.java
@@ -0,0 +1,29 @@
+package com.artichourey.insighthub.serviceImpl;
+
+import java.util.Collections;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.artichourey.insighthub.entities.User;
+import com.artichourey.insighthub.exception.ResourceNotFoundException;
+import com.artichourey.insighthub.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user=userRepository.findByName(username).orElseThrow(()-> new ResourceNotFoundException("user not found with this"+username));
+		
+		return new org.springframework.security.core.userdetails.User(user.getName(),user.getPassword(),Collections.emptyList());
+	}
+
+}
