3afa41d0afb66688f9c7a8a2c74ca73e36b2c9b8
Arti <arti.chourey21@gmail.com>
2026-03-05 21:21:18 +0530

    refactor(user):add a new method findByUserName

--- src/main/java/com/artichourey/insighthub/repositories/UserRepository.java
+++ src/main/java/com/artichourey/insighthub/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.artichourey.insighthub.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.artichourey.insighthub.entities.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+	
+	Optional<User> findByName(String Name);
 
 }
