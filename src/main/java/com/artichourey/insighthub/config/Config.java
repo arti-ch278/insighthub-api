bd9b271dfd7c1a674d52031a2c7a85f45a6e3141
Arti <arti.chourey21@gmail.com>
2026-03-07 20:59:33 +0530

    config: add cors configuration

---
+++ src/main/java/com/artichourey/insighthub/config/Config.java
@@ -0,0 +1,25 @@
+package com.artichourey.insighthub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public WebMvcConfigurer corsConfiguration() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {  
+            	registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*");
+        }
+    };
+
+}
+}
