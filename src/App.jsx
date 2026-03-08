433eee386231881da1d8876e75b368c87dab867c
Arti <arti.chourey21@gmail.com>
2026-03-08 22:08:35 +0530

    chore(ui):Updated App.jsx routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-import Home from "./pages/Home";
 
-export default function App(){
-  return <Home />;
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";          // main posts page
+import CreatePost from "./pages/CreatePost";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthContext";
+import PostDetails from "./pages/PostDetails";
+import EditPost from "./pages/EditPost";   
+import Categories from "./pages/Categories"; 
+
+
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />        
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        {/*  added route for update/edit post */}
+        <Route path="/posts/edit/:id" element={<EditPost />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </AuthProvider>
+  );
 }
