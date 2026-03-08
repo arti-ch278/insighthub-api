cd705d9f319cbd2696445156583d67522e465b8d
Arti <arti.chourey21@gmail.com>
2026-03-08 21:34:32 +0530

    feat(upload-image):add upload image api endpoints for post

---
+++ src/main/java/com/artichourey/insighthub/controllers/PostController.java
@@ -0,0 +1,86 @@
+package com.artichourey.insighthub.controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.artichourey.insighthub.dtos.PostRequestDto;
+import com.artichourey.insighthub.dtos.PostResponseDto;
+import com.artichourey.insighthub.payloads.ApiResponse;
+import com.artichourey.insighthub.service.PostService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostController {
+	
+	private final PostService postService;
+	
+	@PostMapping("/user/{userId}/category/{categoryId}")
+    public ResponseEntity<PostResponseDto> createPost(@Valid @RequestBody 
+    		PostRequestDto postRequestDto, @PathVariable Long userId, @PathVariable Long categoryId) {
+
+        PostResponseDto createdPost = postService.createPost(
+                postRequestDto, userId,categoryId);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<PostResponseDto> updatePost(
+            @PathVariable Long postId,
+            @Valid @RequestBody PostRequestDto postRequestDto) {
+
+        PostResponseDto updatedPost = postService.updatePost(postId, postRequestDto);
+        return ResponseEntity.ok(updatedPost);
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostResponseDto> getPostById(@PathVariable Long postId) {
+        PostResponseDto post = postService.getPostById(postId);
+        return ResponseEntity.ok(post);
+    }
+    
+    @GetMapping
+    public ResponseEntity<Page<PostResponseDto>> getAllPosts(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "10") int pageSize) {
+
+        Page<PostResponseDto> posts = postService.getAllPosts(pageNumber, pageSize);
+        return ResponseEntity.ok(posts);
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<ApiResponse> deletePost(@PathVariable Long postId) {
+        postService.deletePost(postId);
+        return ResponseEntity.ok(new ApiResponse("Post deleted successfully", true));
+    }
+    @GetMapping("/summary")
+    public ResponseEntity<List<PostResponseDto>> getAllPostSummary(){
+    	return ResponseEntity.ok(postService.getAllPostSummary());
+    }
+    
+    @PostMapping("/{postId}/upload-image")
+    public ResponseEntity<PostResponseDto> uploadImage(@PathVariable Long postId ,@RequestParam("file") MultipartFile file) throws IOException{
+    	
+    	return ResponseEntity.ok(postService.uploadImage(postId, file));
+    	
+    }
+    	
+}
