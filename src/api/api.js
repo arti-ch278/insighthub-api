bf04fad600cbdafa0d3b20095bb79d25e5bcabf3
Arti <arti.chourey21@gmail.com>
2026-03-08 22:12:04 +0530

    chore(ui): Updated App.jsx routing

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,25 @@
+
 import axios from "axios";
 
+// Users API
 export const api = axios.create({
   baseURL: "http://localhost:8080/api/users",
 });
+
+// Posts API
+export const api1 = axios.create({
+  baseURL: "http://localhost:8080/api/posts",
+});
+
+// Auth API
+export const api2 = axios.create({
+  baseURL: "http://localhost:8080/api/auth",
+});
+// Comment API
+export const api3 = axios.create({
+  baseURL: "http://localhost:8080/api/comments",
+});
+// Categories API 
+export const api4 = axios.create({ 
+  baseURL: "http://localhost:8080/api/categories"
+ });
