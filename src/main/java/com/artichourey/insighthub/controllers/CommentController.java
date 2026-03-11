db282f53225a3c76c6318befffe0b8a85f8b25aa
Arti <arti.chourey21@gmail.com>
2026-03-11 16:18:56 +0530

    feat:Only comment creator can delete his comment only and annotate comment endpoints with @Operation/@Tag for Swagger

--- src/main/java/com/artichourey/insighthub/controllers/CommentController.java
+++ src/main/java/com/artichourey/insighthub/controllers/CommentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,50 +14,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.artichourey.insighthub.dtos.CommentRequestDto;
 import com.artichourey.insighthub.dtos.CommentResponseDto;
-import com.artichourey.insighthub.entities.User;
-import com.artichourey.insighthub.exception.ResourceNotFoundException;
 import com.artichourey.insighthub.payloads.ApiResponse;
-import com.artichourey.insighthub.repositories.UserRepository;
 import com.artichourey.insighthub.service.CommentService;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/comments")
 @RequiredArgsConstructor
+@Tag(name = "Comment APIs", description = "Operations related to comments")
 public class CommentController {
 	
 	private final CommentService commentService;
-	private final UserRepository userRepository;
 	
+	@Operation(summary = "Add a comment to a post")
 	@PostMapping("/{postId}")
-	public ResponseEntity<?> addCreate(@PathVariable Long postId, 
+	@PreAuthorize("isAuthenticated()")
+	public ResponseEntity<?> createComment(@PathVariable Long postId, 
 			@Valid @RequestBody CommentRequestDto dto, @AuthenticationPrincipal UserDetails userDetails ){
-		if(userDetails==null) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login required for comment");
-		}
-		User user=userRepository.findByName(userDetails.getUsername()).orElseThrow(()->new ResourceNotFoundException("user not found"));
-		CommentResponseDto saved=commentService.addComment(postId, dto, user);
+		
+		CommentResponseDto saved=commentService.addComment(postId, dto, userDetails.getUsername() );
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
 		
 	}
+	@Operation(summary = "Get comments by post ID")
 	@GetMapping("/{postId}")
 	public ResponseEntity<List<CommentResponseDto>> getComment(@PathVariable Long postId){
 		List<CommentResponseDto> list =commentService.getCommentsByPostId(postId);
 		return ResponseEntity.ok(list);
 		
 	}
-	
+	@Operation(summary = "Delete a comment by comment ID")
+	//AuthenticationPrincipal to get current logged-in user
 	@DeleteMapping("/{commentId}")
-	public ResponseEntity<ApiResponse> deleteComment(@PathVariable Long commentId){
-		commentService.deleteComment(commentId);
-		return ResponseEntity.ok(new ApiResponse("user deleted successfully",true));
-		
+	public ResponseEntity<ApiResponse> deleteComment(
+	        @PathVariable Long commentId,
+	        @AuthenticationPrincipal UserDetails currentUser 
+	) {
+	    commentService.deleteComment(commentId, currentUser.getUsername());
+	    return ResponseEntity.ok(new ApiResponse("Comment deleted successfully", true));
 	}
 
 }
