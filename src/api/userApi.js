6327f0522c26486624b089068c6619f0ee098202
Arti <arti.chourey21@gmail.com>
2026-03-25 22:29:40 +0530

    (user-ui): remove pagination from user and add only user List

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,6 +4,7 @@ import { api } from "./api"
 
 export const createUser = (user) => api.post("", user);
 export const getUsers = () => api.get("");
+//export const getUsers = (page = 0, size = 10) => api.get(`?page=${page}&size=${size}`);
 export const getUserById = (id) => api.get(`/${id}`);
 export const updateUser = (id, user) => api.put(`/${id}`, user);
 export const deleteUser = (id) => api.delete(`/${id}`);
