eea2e1c55c468c1ad019a0e36e0a05263cf6de81
Arti <arti.chourey21@gmail.com>
2026-03-26 13:55:29 +0530

    feat(post-ui): add route for edit posts and create and edit categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Posts from "./pages/Posts";          // main posts page
+import Posts from "./pages/Posts";
 import CreatePost from "./pages/CreatePost";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import AuthProvider from "./context/AuthContext";
 import PostDetails from "./pages/PostDetails";
-import EditPost from "./pages/EditPost";   
-import Categories from "./pages/Categories"; 
-
-
+import EditPost from "./pages/EditPost";
+import Categories from "./pages/Categories";
 
 export default function App() {
   return (
     <AuthProvider>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Posts />} />        
-        <Route path="/create-post" element={<CreatePost />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/posts/:id" element={<PostDetails />} />
-        {/*  added route for update/edit post */}
-        <Route path="/posts/edit/:id" element={<EditPost />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+
+      {/* PAGE CONTAINER FIX */}
+      <main className="w-full flex justify-center px-4">
+        <div className="w-full max-w-4xl">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/posts/edit/:id" element={<EditPost />} />
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </div>
+      </main>
     </AuthProvider>
   );
 }
