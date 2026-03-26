277b9f129a5811fe2347ef2d1df12356eb543c50
Arti <arti.chourey21@gmail.com>
2026-03-26 13:26:17 +0530

    feat(post-ui):add post list api to show the list of posts

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -4,26 +4,35 @@ import { api1 } from "./api";
 export const createPost = (post, userId, categoryId) =>
   api1.post(`/user/${userId}/category/${categoryId}`, post);
 
-// UPDATE
-export const updatePost = (post, postId) =>
-  api1.put(`/${postId}`, post);
+// UPDATE 
+export const updatePost = (postData, postId, file) => {
+  if (file) {
+    // If uploading new image
+    const formData = new FormData();
+    formData.append("title", postData.title);
+    formData.append("content", postData.content);
+    formData.append("file", file); // new image file
+    return api1.put(`/${postId}`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+  } else {
+    // No new image, send JSON with old imageName
+    return api1.put(`/${postId}`, postData);
+  }
+};
 
 // GET SINGLE
-export const getPostById = (postId) =>
-  api1.get(`/${postId}`);
+export const getPostById = (postId) => api1.get(`/${postId}`);
 
 // DELETE
-export const deletePost = (postId) =>
-  api1.delete(`/${postId}`);
+export const deletePost = (postId) => api1.delete(`/${postId}`);
 
 // GET ALL POST SUMMARY
-
 export const getAllPostSummary = () => api1.get("/summary");
-//  Upload post image
 
+// UPLOAD POST IMAGE 
 export const uploadPostImage = (postId, file) => {
   const formData = new FormData();
-  formData.append("file", file);   
-
+  formData.append("file", file);
   return api1.post(`/${postId}/upload-image`, formData);
 };
