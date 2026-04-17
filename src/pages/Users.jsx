5ce893c8f95cfee077b3627b6ba5c833ce926647
Arti <arti.chourey21@gmail.com>
2026-04-17 18:39:26 +0530

    feat: add Users page for user creation and info display

---
+++ src/pages/Users.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import UserForm from "../components/UserForm";
+import UserList from "../components/UserList";
+
+
+function Users (){
+
+  const [reload,setReload] = useState(false);
+
+  return (
+    
+    <div className="grid grid-cols-2 gap-4 p-6">
+      <UserForm onSuccess={() => setReload(!reload)}></UserForm>
+      <UserList reload={reload} />
+    </div>
+  );
+}
+
+export default Users;
